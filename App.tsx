fd3d3b806aedad7d9faddf99784476b287db44c8
Famousming <36706893+kpstar@users.noreply.github.com>
2021-12-23 07:38:17 +0800

    First commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Button, Text, Container } from 'native-base';
+import { useState } from 'react';
+import { responsiveWidth } from 'react-native-responsive-dimensions';
 
 export default function App() {
+
+  const [Clicks, setClicks] = useState(0);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <Container style={styles.container}>
+      <Text style={styles.title}>MakrVerse Counter</Text>
+      <Text style={styles.counter}>{Clicks}</Text>
+      <Button style={styles.button} block primary rounded onPress={() => setClicks(Clicks + 1)}><Text>Increment</Text></Button>
+      <Button style={styles.button} block light rounded disabled={Clicks==0} onPress={() => setClicks(Clicks - 1)}><Text>Decrement</Text></Button>
+      <Button style={styles.button} block warning rounded onPress={() => setClicks(0)}><Text>Reset</Text></Button>
       <StatusBar style="auto" />
-    </View>
+    </Container>
   );
 }
 
@@ -17,4 +27,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#316BEA',
+    marginBottom: 20,
+    fontSize: 25
+  },
+  counter: {
+    color: '#F00',
+    fontSize: 20,
+    marginBottom: 20
+  },
+  button: {
+    marginLeft: responsiveWidth(10),
+    width: responsiveWidth(80),
+    marginVertical: 10
+  }
 });
